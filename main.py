ec6c45b502ee629adc211d8abd46d2092a68c23a
All-Chronical <a.c.a11.creator@gmail.com>
2026-09-17 15:43:34 -0400

    feat: box detection

--- main.py
+++ main.py
@@ -8,11 +8,16 @@ while True:
 
     frame  = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     frame  = cv2.GaussianBlur(frame, (5,5), 0)
+    frame = cv2.Canny(frame, 50, 150)
 
-    corners = cv2.goodFeaturesToTrack(frame, 50, 0.5, 10)
-    if corners is not None:
-        for x, y in corners.reshape(-1, 2).astype(int):
-            cv2.circle(frame, (x, y), 4, (255, 0, 0), -1)
+    contours, _ = cv2.findContours(frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    card  = max(contours, key=cv2.contourArea)
+    rect  = cv2.minAreaRect(card)
+    box   = cv2.boxPoints(rect).astype(int)   # 4 corners of the fit
+
+    frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR)   # red now possible
+
+    cv2.drawContours(frame, [box], 0, (0, 0, 255), 3)
 
     cv2.imshow("OpenIPD Alpha", frame)
     if cv2.waitKey(1) == 27:
