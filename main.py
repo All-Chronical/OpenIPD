99a76f45a69fa6b46202ab610277efc30d3b6de9
All-Chronical <a.c.a11.creator@gmail.com>
2026-09-17 15:03:28 -0400

    feat: simplify camera feed

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ camera = cv2.VideoCapture(0)
 while True:
     (ret, frame) = camera.read()
 
+    frame  = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    frame  = cv2.GaussianBlur(frame, (5,5), 0)
+    frame = cv2.Canny(frame, 50, 150)
+
     cv2.imshow("OpenIPD Alpha", frame)
     if cv2.waitKey(1) == 27:
         break
