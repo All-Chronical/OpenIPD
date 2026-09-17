a01eae1f373a7536881a9c3d6186cfbf3282a43d
All-Chronical <a.c.a11.creator@gmail.com>
2026-09-17 15:19:12 -0400

    feat: corner detection attempt

--- main.py
+++ main.py
@@ -8,7 +8,11 @@ while True:
 
     frame  = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     frame  = cv2.GaussianBlur(frame, (5,5), 0)
-    frame = cv2.Canny(frame, 50, 150)
+
+    corners = cv2.goodFeaturesToTrack(frame, 50, 0.5, 10)
+    if corners is not None:
+        for x, y in corners.reshape(-1, 2).astype(int):
+            cv2.circle(frame, (x, y), 4, (255, 0, 0), -1)
 
     cv2.imshow("OpenIPD Alpha", frame)
     if cv2.waitKey(1) == 27:
