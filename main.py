17e01ae4f87e736a1c8c228da6a20c87796ed5c7
All-Chronical <a.c.a11.creator@gmail.com>
2026-09-23 19:51:33 -0400

    feat: replace contour detection with line detection for more flexiblity

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 import cv2
-import numpy
+import numpy as np
 
 camera = cv2.VideoCapture(0)
 
 while True:
     (ret, frame) = camera.read()
 
-    frame  = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    frame  = cv2.GaussianBlur(frame, (5,5), 0)
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    frame = cv2.GaussianBlur(frame, (5, 5), 0)
     frame = cv2.Canny(frame, 50, 150)
 
-    contours, _ = cv2.findContours(frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    card  = max(contours, key=cv2.contourArea)
-    rect  = cv2.minAreaRect(card)
-    box   = cv2.boxPoints(rect).astype(int)   # 4 corners of the fit
+    lines = cv2.HoughLinesP(frame, 1, np.pi / 180, threshold=50, minLineLength=30, maxLineGap=30)
 
     frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR)   # red now possible
 
-    cv2.drawContours(frame, [box], 0, (0, 0, 255), 3)
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line
+            cv2.line(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
 
     cv2.imshow("OpenIPD Alpha", frame)
     if cv2.waitKey(1) == 27:
